c3550206986f2175a2d4f71438ce533f3a99a007
Philani Gumede <philaningumede@gmail.com>
2022-09-30 14:02:23 +0200

    using chi

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,23 +22,30 @@ func fqaHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w,
 		"<h1>FQA</h1><p>Q: Is there a free version?</p><p>A: Yes we offer a free 30 days trial</p>")
 }
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/fqa":
-		fqaHandler(w, r)
-	default:
-		http.NotFound(w, r)
-	}
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Page Not Found", http.StatusNotFound)
 }
 
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	case "/fqa":
+// 		fqaHandler(w, r)
+// 	default:
+// 		http.NotFound(w, r)
+// 	}
+// }
+
 func main() {
-	// http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", fqaHandler)
+	r.NotFound(notFound)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", r)
 }
