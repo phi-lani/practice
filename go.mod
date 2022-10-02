a5ed666c4716bcd2a9162e649001ecf72e6d5f04
Philani Gumede <philaningumede@gmail.com>
2022-10-03 00:27:04 +0200

    added a controllers package and used closured to execute the template

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/phi-lani/webApp
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -2,48 +2,67 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/phi-lani/webApp/controllers"
 	"github.com/phi-lani/webApp/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	t, err := views.Parse(filepath)
+// func executeTemplate(w http.ResponseWriter, filepath string) {
+// 	t, err := views.Parse(filepath)
 
-	if err != nil {
-		// Invalid information during randering
-		log.Printf("Parsing template: %v", err)
-		http.Error(w, "There was an error executing the templete", http.StatusInternalServerError)
-		return
-	}
+// 	if err != nil {
+// 		// Invalid information during randering
+// 		log.Printf("Parsing template: %v", err)
+// 		http.Error(w, "There was an error executing the templete", http.StatusInternalServerError)
+// 		return
+// 	}
 
-	t.Execute(w, nil)
-}
+// 	t.Execute(w, nil)
+// }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/home.gohtml")
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	executeTemplate(w, "templates/home.gohtml")
+// }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/contact.gohtml")
-}
+// func contactHandler(w http.ResponseWriter, r *http.Request) {
+// 	executeTemplate(w, "templates/contact.gohtml")
+// }
 
-func fqaHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/faq.gohtml")
-}
+// func faqHandler(w http.ResponseWriter, r *http.Request) {
+// 	executeTemplate(w, "templates/faq.gohtml")
+// }
 
-func notFound(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Page Not Found", http.StatusNotFound)
-}
+// func notFound(w http.ResponseWriter, r *http.Request) {
+// 	http.Error(w, "Page Not Found", http.StatusNotFound)
+// }
 
 func main() {
+
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", fqaHandler)
-	r.NotFound(notFound)
+
+	tpl, err := views.Parse("templates/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("templates/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("templates/faq.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+	// r.Get("/", homeHandler)
+	// r.Get("/contact", contactHandler)
+	// r.Get("/faq", faqHandler)
+	// r.NotFound(notFound)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
