ff98e3c70793685e60c937969484bc2e2ab001e4
Philani Gumede <philaningumede@gmail.com>
2022-10-02 21:14:26 +0200

    Routing the homeHandler via an html template

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,7 +11,21 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1> Welcome to my great site!</h1>")
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		// Failing to parse due to invalid information inside the template.
+		log.Printf("Parsing template: %v", err)
+		http.Error(w, "There was an error parsing the templete", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		// Invalid information during randering
+		log.Printf("Parsing template: %v", err)
+		http.Error(w, "There was an error executing the templete", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,26 +35,12 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func fqaHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w,
-		"<h1>FQA</h1><p>Q: Is there a free version?</p><p>A: Yes we offer a free 30 days trial</p>")
+	fmt.Fprint(w, "<h1>FQA</h1><p>Q: Is there a free version?</p><p>A: Yes we offer a free 30 days trial</p>")
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Page Not Found", http.StatusNotFound)
 }
-
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	case "/fqa":
-// 		fqaHandler(w, r)
-// 	default:
-// 		http.NotFound(w, r)
-// 	}
-// }
 
 func main() {
 	r := chi.NewRouter()
