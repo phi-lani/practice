9cbd1b014b8cd608ce22229b8506a60c1ae4b144
Philani Gumede <philaningumede@gmail.com>
2022-11-01 22:19:00 +0200

    Started using CSS Tailwind templates

--- main.go
+++ main.go
@@ -14,16 +14,16 @@ func main() {
 
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.ParseFS(templates.FS, "about.gohtml"))
+	tpl = views.Must(views.ParseFS(templates.FS, "about.gohtml", "tailwind.gohtml"))
 	r.Get("/about", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
 	r.Get("/faq", controllers.FAQ(tpl))
 
 	fmt.Println("Starting the server on :3000...")
